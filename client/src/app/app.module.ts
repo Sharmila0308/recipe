85c915e82468a8422cb2449a68fa3a4fbb7e6f74
Sharmila0308 <parashara.sharmila@gmail.com>
2018-06-10 16:45:55 -0400

    component creations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,53 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ItemComponent } from './item/item.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { FindComponent } from './home/find/find.component';
+import { ListComponent } from './home/list/list.component';
+import { MenuComponent } from './header/menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ItemComponent,
+    ProfileComponent,
+    LoginComponent,
+    FindComponent,
+    ListComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { 
+        path: '', 
+        component: HomeComponent 
+      },
+      { 
+        path: 'itemview', 
+        component: ItemComponent 
+      },
+      { 
+        path: 'profile', 
+        component: ProfileComponent 
+      },
+      { 
+        path: 'login', 
+        component: LoginComponent 
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
